fb2c4795fb875344d2de145a731eed3c9dabd02b
Matteo Redaelli <matteo.redaelli@libero.it>
2011-06-02 14:23:11 +0200

    added db saving support

--- dump_tweets.py
+++ dump_tweets.py
@@ -24,6 +24,7 @@ import HTMLParser
 import json
 import os.path
 import rfc822
+import string
 import sys
 import time
 import urllib
@@ -39,7 +40,7 @@ def clean_string(s):
     s = s.strip()
     return s
 
-def dump_tweets(q, since_id=0, verbose=True, rpp=100, result_type = 'recent'):
+def dump_tweets(q, since_id=0, verbose=True, rpp=100, result_type = 'recent', db_cursor=False, db_table=False):
     base_url = "http://search.twitter.com/search.json"
     query = "?" + urllib.urlencode({'q' : q,
                                     'since_id'    : since_id,
@@ -60,10 +61,14 @@ def dump_tweets(q, since_id=0, verbose=True, rpp=100, result_type = 'recent'):
             from_user = clean_string(tweet["from_user"])
             ##from_user_id = clean_string(tweet["from_user_id"])
             text = clean_string(tweet["text"])
-            iso_language_code = clean_string(tweet["iso_language_code"])
+            iso_language_code = ""
+            ##iso_language_code = tweet["iso_language_code"]
             row = str(id) + " : " + str(timestamp) + " : " + from_user + " : " + text + " : " + iso_language_code
             print row.encode('utf8')
-        
+            if db_cursor != False:
+                sql_statement = "insert into %s (id, from_user, timestamp, text, iso_language_code) values (%d, '%s', %d, '%s', '%s')" %  (db_table, id, from_user,  timestamp, text.replace("'","\\'"), iso_language_code)
+                db_cursor.execute(sql_statement.encode('utf8'))
+                
         if "next_page" in json_response.keys():
             query = json_response["next_page"]
         else:
@@ -74,15 +79,16 @@ def dump_tweets(q, since_id=0, verbose=True, rpp=100, result_type = 'recent'):
     return max_id
 
 
-def rep_dump_tweets(q, since_id=0, sleep=3600, verbose=True, rpp=100, result_type = 'recent', filename="tweets.id"):
+def rep_dump_tweets(q, since_id=0, sleep=3600, verbose=True, rpp=100, result_type = 'recent', filename=False, db_cursor=False, db_table=False):
     while True:
         if verbose:
             print >> sys.stderr, "since_id = " + str(since_id)
-        max_id = dump_tweets(q=q, since_id=since_id, verbose=verbose)
+        max_id = dump_tweets(q=q, since_id=since_id, verbose=verbose, db_cursor=db_cursor, db_table=db_table)
         if verbose:
             print >> sys.stderr, "max_id = " + str(max_id)
         since_id = max_id
-        id_to_file(since_id, filename)
+        if filename != False:
+            id_to_file(since_id, filename)
         if sleep == 0:
             break
         time.sleep(sleep)
@@ -105,7 +111,7 @@ def id_to_file(id, filename):
     
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "fhi:q:s:v", ["file", "help", "query=", "since_id=", "sleep="])
+        opts, args = getopt.getopt(sys.argv[1:], "d:fhi:q:s:v", ["db:", "file", "help", "query=", "since_id=", "sleep="])
     except getopt.GetoptError, err:
         usage()
         sys.exit(2)
@@ -113,12 +119,15 @@ def main():
     q = ""
     since_id = 0  
     sleep = 0
+    db = False
     filename = False
     verbose = False
     
     for o, a in opts:
         if o == "-v":
             verbose = True
+        elif o in ("-d", "--database"):
+            db = string.split(a,":")
         elif o in ("-f", "--file"):
             filename = True
         elif o in ("-h", "--help"):
@@ -144,8 +153,43 @@ def main():
             if verbose:
                 print >> sys.stderr, "Reading since_id from file " + filename
             since_id = id_from_file(filename)
-        
-    rep_dump_tweets(q=q, since_id=since_id, sleep=sleep, verbose=verbose, filename=filename)
-        
+
+    db_cursor = False
+    db_table = False
+    
+    if not (db == False):
+        if len(db) != 6:
+            print("-d option is wrong.")
+            usage()
+            sys.exit(4)
+            
+        db_host = db[0]
+        db_port = db[1]
+        db_name = db[2]
+        db_user = db[3]
+        db_pass = db[4]
+        db_table = db[5]
+
+ 
+        try:
+            conn = MySQLdb.connect (host = db_host,
+                                    user = db_user,
+                                    passwd = db_pass,
+                                    db = db_name)
+            db_cursor = conn.cursor ()
+        except MySQLdb.Error, e:
+            print "Error %d: %s" % (e.args[0], e.args[1])
+            
+    
+    rep_dump_tweets(q=q,
+                    since_id=since_id,
+                    sleep=sleep,
+                    verbose=verbose,
+                    filename=filename,
+                    db_cursor = db_cursor,
+                    db_table=db_table)
+    db_cursor.close()
+    conn.close()
+    
 if __name__ == "__main__":
     main()
